c318c41294b51ce1c73fd27f4ab1a43ec0137b42
Aaron Kriegman <aaronkplus2@gmail.com>
2023-08-11 16:51:50 +0100

    added godot

---
+++ godot/input.gd
@@ -0,0 +1,65 @@
+extends Node3D
+
+@export var joystick: VirtualJoystick
+
+@export var dampening = 0.1
+@export var force = 4.0
+@export var sensitivity = 200
+var velocity = Vector3.ZERO
+
+func _process(delta):
+	var acceleration = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		acceleration += Vector3.LEFT
+	if Input.is_action_pressed("move_right"):
+		acceleration += Vector3.RIGHT
+	if Input.is_action_pressed("move_forward"):
+		acceleration += Vector3.FORWARD
+	if Input.is_action_pressed("move_back"):
+		acceleration += Vector3.BACK
+	if Input.is_action_pressed("move_up"):
+		acceleration += Vector3.UP
+	if Input.is_action_pressed("move_down"):
+		acceleration += Vector3.DOWN
+	
+	if joystick.is_pressed:
+		acceleration += \
+			joystick.output.x * Vector3.RIGHT + \
+			joystick.output.y * Vector3.BACK
+	
+	acceleration = acceleration.normalized().rotated(Vector3.UP, rotation.y) * force
+	velocity += acceleration * delta
+	velocity *= dampening ** delta
+	position += velocity * delta
+	
+func _input(event):
+	if event is InputEventMouseMotion and not joystick.is_pressed:
+		rotation.y -= event.relative.x / sensitivity
+		rotation.x -= event.relative.y / sensitivity
+
+func _on_up_button_down():
+	Input.action_press("move_up")
+
+func _on_up_button_up():
+	Input.action_release("move_up")
+
+func _on_down_button_down():
+	Input.action_press("move_down")
+
+func _on_down_button_up():
+	Input.action_release("move_down")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
